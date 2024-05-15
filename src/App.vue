3e90a018589271ba61ec5b8ee2fd14015990d83c
tanny <tannyapriliani271@gmail.com>
2024-05-15 20:29:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ToDoList />
+</template>
+
+<script>
+import ToDoList from "@/components/ToDoList";
+
+export default {
+  name: 'App',
+
+  components: {
+    ToDoList
+  }
+}
+
+</script>
+
+<style>
+body{
+  background-image:url(./assets/cb.jpeg);
+background: cover;
+background-size: cover;
+background-repeat: no-repeat;
+}
+</style>
